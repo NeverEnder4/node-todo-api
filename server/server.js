a52508c08f988973fd0d63f1b389039aaefb4fc3
Tony <apettigrew.wsdev@gmail.com>
2018-07-12 11:42:01 -0700

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 
 var { mongoose } = require('./db/mongoose');
@@ -29,6 +30,39 @@ app.get('/todos', (req, res) => {
     });
 
 });
+
+//GET /todos/_id
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            res.status(404).send(todo);
+        }
+
+        res.send({ todo });
+
+    }, (e) => {
+        res.status(400).send();
+    })
+
+
+    //Valid id using isValid
+        // 404 - send back empty send
+    // findById
+        // success
+            //if todo - send it back
+            //if not todo - send back 404 with empty body
+        // error
+            //400 - send back nothing - error could send back private information
+
+    
+})
 
 
 
