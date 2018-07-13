63d3fa4b7ef210650c947673e0e118ff711c1835
Tony <apettigrew.wsdev@gmail.com>
2018-07-12 20:07:15 -0700

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -17,17 +17,17 @@ app.post('/todos', (req, res) => {
         text: req.body.text
     });
     todo.save().then((doc) => {
-        res.send(doc);
+        return res.send(doc);
     }, (e) => {
-        res.status(400).send(e);
+        return res.status(400).send(e);
     })
 });
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
-        res.send({ todos })
+        return res.send({ todos })
     }, (e) => {
-        res.status(400).send(e);
+        return res.status(400).send(e);
     });
 
 });
@@ -41,26 +41,32 @@ app.get('/todos/:id', (req, res) => {
 
     Todo.findById(id).then((todo) => {
         if (!todo) {
-            res.status(404).send(todo);
+            return res.status(404).send(todo);
         }
 
-        res.send({ todo });
+        return res.send({ todo });
 
     }, (e) => {
-        res.status(400).send();
+        return res.status(400).send();
     })
+})
 
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
 
-    //Valid id using isValid
-        // 404 - send back empty send
-    // findById
-        // success
-            //if todo - send it back
-            //if not todo - send back 404 with empty body
-        // error
-            //400 - send back nothing - error could send back private information
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
 
-    
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(!todo) {
+           return res.status(404).send();
+        }
+
+       return res.send(todo);
+    }, (e) => {
+       return res.status(400).send();
+    })
 })
 
 
